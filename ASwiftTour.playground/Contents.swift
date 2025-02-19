29e6b0e13904386d56cf68952c0649af72aa6644
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-02-19 14:38:03 +0300

    Functions and Closures.

--- ASwiftTour.playground/Contents.swift
+++ ASwiftTour.playground/Contents.swift
@@ -151,3 +151,90 @@ for i in 0..<4 {
 }
 print(total)
 // Prints "6"
+
+
+// Functions and Closures
+
+func greet(person: String, day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+
+greet(person: "Bob", day: "Tuesday")
+
+func greet2(_ person: String, on day: String) -> String {
+    return "Hello \(person), today is \(day)"
+}
+
+greet2("John", on: "Wednesday")
+
+func calculateStatictics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        
+        sum += score
+    }
+    
+    return (min, max, sum)
+}
+
+let statistics = calculateStatictics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+// Prints "120"
+
+print(statistics.2)
+// Prints "120"
+
+
+func returnFifteen() -> Int {
+    var y = 10
+    func add() {
+        y += 5
+    }
+    add()
+    return y
+}
+
+returnFifteen()
+
+func makeIncrementer() -> ((Int) -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    
+    return addOne
+}
+
+var increment = makeIncrementer()
+increment(7)
+
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+
+var numbers = [20, 19, 7, 12]
+hasAnyMatches(list: numbers, condition: lessThanTen)
+
+let mappedNumbers = numbers.map { number in 3 * number }
+print(mappedNumbers)
+// Prints "[60, 57, 21, 36]"
+
+let sortedNumbers = numbers.sorted{ $0 > $1 }
+print(sortedNumbers)
+// Prints "[20, 19, 12, 7]"
