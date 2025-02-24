2ad3018fe063ac4f45c0650c40737fe0ad0a36dc
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-02-24 12:06:03 +0300

    Concurrency ini

--- ASwiftTour.playground/Contents.swift
+++ ASwiftTour.playground/Contents.swift
@@ -441,3 +441,51 @@ func fetchUserID(from server: String) async -> Int {
     return 501
 }
 
+func fetchUsername(from server: String) async -> String {
+    let userID = await fetchUserID(from: server)
+    if userID == 501 {
+        return "John Appleseed"
+    }
+    return "Guest"
+}
+
+func connectUser(to server: String) async {
+    async let userID = fetchUserID(from: server)
+    async let username = fetchUsername(from: server)
+    let greeting = await "Hello \(username), user ID \(userID)"
+    print(greeting)
+}
+
+Task {
+    await connectUser(to: "primary")
+}
+
+// Prints "Hello Guest, user ID 97"
+
+let userIDs = await withTaskGroup(of: Int.self) { group in
+    for server in ["primary", "secondary", "development"] {
+        group.addTask {
+            return await fetchUserID(from: server)
+        }
+    }
+    
+    var results: [Int] = []
+    for await result in group {
+        results.append(result)
+    }
+    return results
+}
+
+actor ServerCoonection {
+    var server: String = "primary"
+    private var activeUsers: [Int] = []
+    func connect() async -> Int {
+        let userID = await fetchUserID(from: server)
+        // ... communicate with server ...
+        activeUsers.append(userID)
+        return userID
+    }
+}
+
+let server = ServerCoonection()
+let userID = await server.connect()
