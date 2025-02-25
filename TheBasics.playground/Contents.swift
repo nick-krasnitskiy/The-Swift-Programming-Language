86149e457dddd01e4824f9d193632e8cc13bf831
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-02-25 15:33:41 +0300

    Declaring Constants and Variables.

---
+++ TheBasics.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// The Basics
+
+// Constants and Variables
+
+let maximumNumberOfLoginAttemps = 10
+var currentLoginAttempt = 0
+
+var environment = "developmnet"
+let maximumNumberOfLoginAttemps2: Int
+// maximumNumberOfLoginAttemps2 has no value yet.
+
+if environment == "developmnet" {
+    maximumNumberOfLoginAttemps2 = 100
+} else {
+    maximumNumberOfLoginAttemps2 = 10
+}
+// Now maximumNumberOfLoginAttempts has a value, and can be read.
+
+var x = 0.0, y = 0.0, z = 0.0 // declare multiple variables on a single line
+
+
+
