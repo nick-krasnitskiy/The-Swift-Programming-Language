df9452e016e9abe6fbcbe9dc1cdfae74385efb2e
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-03-01 18:59:42 +0300

    Integers Bounds.

--- TheBasics.playground/Contents.swift
+++ TheBasics.playground/Contents.swift
@@ -63,3 +63,12 @@ but is written over multiple lines.*/
 
 let cat = "🐱"; print(cat)
 // Prints "🐱"
+
+// Integers Bounds
+
+let minValue = UInt8.min // minValue is equal to 0, and is of type UInt8
+let maxValue = UInt8.max // maxValue is equal to 255, and is of type UInt8
+
+
+
+
