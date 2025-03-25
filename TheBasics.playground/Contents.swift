867c9112e3ec5276e8346f9e8d020e5a137806d0
Nick Krasnitskiy <nick.krasnitskiy@gmail.com>
2025-03-25 12:08:47 +0300

    Type Safety and Type Inference.

--- TheBasics.playground/Contents.swift
+++ TheBasics.playground/Contents.swift
@@ -69,6 +69,16 @@ let cat = "🐱"; print(cat)
 let minValue = UInt8.min // minValue is equal to 0, and is of type UInt8
 let maxValue = UInt8.max // maxValue is equal to 255, and is of type UInt8
 
+// Type Safety and Type Inference
+
+let meaningOflife = 42
+// meaningOfLife is inferred to be of type Int
+
+let pi = 3.14159
+// pi is inferred to be of type Double
+
+let anotherPi = 3 + 0.14159
+// anotherPi is also inferred to be of type Double
 
 
 
